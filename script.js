9a6f6b188d46be29c02c2e4a1a14145dd8fd7866
PrinceYadav-001 <princeyadav6759@gmail.com>
2026-06-21 00:19:48 +0530

    Add Keyboard Support

--- script.js
+++ script.js
@@ -10,27 +10,44 @@ let num2 = "";
 
 //setup
 let operators = ["+", "-", "×", "÷"];
-let validKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "/", "*", "=", "."];
+let validKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "×", "÷", "=", ".", "Backspace"];
 
 //selecting DOM
 const buttons = document.querySelectorAll(".btn");
 const display = document.querySelector(".display");
 
 //adding eventListeners
-buttons.forEach(btn => btn.addEventListener("click", (e) => HandleInput(e)));
+buttons.forEach(btn => btn.addEventListener("click", (e) => HandleInput(e.target.textContent)));
+document.addEventListener("keydown", (e) => HandleInput(e.key));
 document.querySelector(".ac-btn").addEventListener("click", () => reset());
 
 
-
-function HandleInput(e)
+function HandleInput(text)
 {
+    //mapping keys to value
+    if(text === "Enter")
+        text = "=";
 
-    let text = e.target.textContent;
-    console.log(text);
+    if(text === "/")
+        text = "÷";
+
+    if(text === "*")
+        text = "×"
+
+    if(validKeys.includes(text) === false)
+    {
+        return;
+    }
+
+    if (text == "Backspace")
+    {
+        HandleBackspace();
+        return;
+    }
 
     if(isVolatile)
     {
-        if(display.textContent.trim() == zeroDivText)
+        if(display.textContent.trim() === zeroDivText)
             reset();
         else if (operators.includes(text))
             state = 1;
@@ -53,8 +70,7 @@ function HandleInput(e)
             break;
     }
 
-    let fullText = num1 + " " + operator + " " + num2;
-    display.textContent = fullText;
+    UpdateDisplay();
 }
 
 function ExecuteState0(text, substate = "execute")
@@ -100,17 +116,20 @@ function ExecuteState1(text, substate = "execute")
 {
 
     //entry
-    if(substate == "entry")
+    if(substate === "entry")
     {
         operator = text;
         return;
     }
 
     //transition condition
-    if (operators.includes(text) == false)
+    if (operators.includes(text) === false)
     {
-        ExecuteState2(text, "entry");
-        state = 2;
+        if(text !== "=")
+        {
+            state = 2;
+            ExecuteState2(text, "entry");
+        }
         return;
     }
 
@@ -120,21 +139,21 @@ function ExecuteState1(text, substate = "execute")
 
 function ExecuteState2(text, substate = "execute")
 {
-    if (text == ".")
+    if (text === ".")
     {
         num2 = AddDot(num2);
         return;
     }
 
     //entry
-    if(substate == "entry")
+    if(substate === "entry")
     {
         num2 = text;
         return;
     }
 
     //transition condition
-    if (text == "=")
+    if (text === "=")
     {
         isVolatile = true;
         state = 0;
@@ -159,14 +178,53 @@ function ExecuteState2(text, substate = "execute")
 
 
 //functions
+function HandleBackspace()
+{
+    if(state === 0)
+    {
+        num1 = num1.slice(0, -1);
+        UpdateDisplay();
+    }
+    else if (state === 1)
+    {
+        operator = "";
+        state = 0;
+        UpdateDisplay();
+    }
+    else if (state === 2)
+    {
+        num2 = num2.slice(0, -1);
+        if(num2 === "")
+        {
+            state = 1;
+        }
+        UpdateDisplay();
+    }
+}
 
 function reset()
 {
-    display.textContent = "";
+    clearDisplay();
+    resetVariables();
+}
+
+function resetVariables()
+{
     state = 0;
     num1 = "";
     operator = "";
     num2 = "";
+}
+
+function clearDisplay()
+{
+    display.textContent = "";
+}
+
+function UpdateDisplay()
+{
+    let fullText = num1 + " " + operator + " " + num2;
+    display.textContent = fullText;
 }
 
 function operate(a, operator, b)
